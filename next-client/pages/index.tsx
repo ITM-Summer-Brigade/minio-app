1061dbf10d4a64eb87cca677fb26d5579b69724d
Julaam Diop <jeebe.diop@gmail.com>
2022-08-18 01:17:59 -0400

    Create next-app and install tailwind

---
+++ next-client/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className="">
+      <header className="flex flex-col justify-center items-center">
+        <h1 className="text-lg text-yellow-700">Minio File App</h1>
+        <h2 className="">Some text...</h2>
+      </header>
+    </main>
+  );
+};
+
+export default Home;
